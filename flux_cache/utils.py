9904c49e525fa3fe1c70f957d62e31edf298aaa3
alvinshaita <alvinshaita@gmail.com>
2026-03-24 19:19:54 +0300

    feat: normalize all args

--- flux_cache/utils.py
+++ flux_cache/utils.py
@@ -1,7 +1,19 @@
 import hashlib
+import inspect
 import pickle
 
 def generate_cache_key(func, args, kwargs) -> str:
-	key_data = (func.__module__, func.__name__, args, kwargs)
+	sig = inspect.signature(func)
+	bound = sig.bind(*args, **kwargs)
+	bound.apply_defaults()
+
+	normalized_args = tuple(bound.arguments.items())
+
+	key_data = (
+		func.__module__,
+		func.__name__,
+		normalized_args
+	)
+
 	raw_data = pickle.dumps(key_data)
 	return hashlib.sha256(raw_data).hexdigest()
