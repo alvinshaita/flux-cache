4cb0a7150c68abe8794c1e756e1f5091bb07fae2
alvinshaita <alvinshaita@gmail.com>
2026-03-25 07:46:49 +0300

    feat: implement stampede protection

---
+++ flux_cache/strategy/__init__.py
@@ -0,0 +1,1 @@
+from .stampede import StampedeProtection
---
+++ flux_cache/strategy/stampede.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+import threading
+
+class StampedeProtection:
+	def __init__(self):
+		self._global_lock = threading.Lock()
+		self._locks = {}
+
+	def _get_lock(self, key):
+		with self._global_lock:
+			if key not in self._locks:
+				self._locks[key] = threading.Lock()
+			return self._locks[key]
+
+	@contextmanager
+	def lock(self, key):
+		lock = self._get_lock(key)
+		lock.acquire()
+		try:
+			yield
+		finally:
+			lock.release()
